06c238d5d4e48d35b6dd55a90d6a86e6d4afd334
MohammadSako <71136910+MohammadSako@users.noreply.github.com>
2022-10-09 17:37:29 +0300

    some fix done..

--- app.js
+++ app.js
@@ -85,7 +85,6 @@ passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
   res.locals.currentUser = req.user;
-  
   res.locals.success = req.flash('success');
   res.locals.error = req.flash('error');
   res.locals.session = req.session;//cart
