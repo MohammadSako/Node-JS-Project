c377f0794e5463b8cfa0ee71c300a8974096b45b
MohammadSako <71136910+MohammadSako@users.noreply.github.com>
2022-10-12 13:52:33 +0300

    simple checkout is done

--- app.js
+++ app.js
@@ -65,7 +65,7 @@ const sessionConfig = {
   saveUninitialized: true,
   cookie: {
       httpOnly: true,
-      secure: true, //only when deploying 
+      // secure: true, //only when deploying 
       expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
       maxAge: 1000 * 60 * 60 * 24 * 7
   }
